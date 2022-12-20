4aab35e581857dd0cce69d13a939a6ad108311f7
kundan singh <kundansingh@kundans-MBP.bbrouter>
2022-12-20 17:09:26 +0530

    Created layout of the beckn exp

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h3>Beckn Experience center</h3>
+    <div className="beckn-app">
+      <div className="beckn-app-container">
+        <h3>Beckn Experience center</h3>
+      </div>
     </div>
   );
 }
