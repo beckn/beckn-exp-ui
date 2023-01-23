db2c8f3d30e59fd1b853c7fd0037be48eadc94e7
parmesh70750 <parmesh.kandregula@eminds.ai>
2023-01-23 22:16:56 +0530

    fixed active colours

--- src/common/FlowChart/NodeComponent.tsx
+++ src/common/FlowChart/NodeComponent.tsx
@@ -24,9 +24,11 @@ const NodeAsHandleFlow: React.FC = () => {
   const fetchEvent = async () => {
     try {
       const res = await getEvent();
-      setEvents(res?.events[0].event);
-      const firstResponseOfAPI = res?.events[1].event.eventMessage.eventCode;
-      const secondResponseOfAPI = res?.events[0].event.eventMessage.eventCode;
+      await setEvents(res?.events[0].event);
+      const firstResponseOfAPI = await res?.events[1].event.eventMessage
+        .eventCode;
+      const secondResponseOfAPI = await res?.events[0].event.eventMessage
+        .eventCode;
 
       if (
         (firstResponseOfAPI === ids.searchBroadCast &&
@@ -40,9 +42,9 @@ const NodeAsHandleFlow: React.FC = () => {
         (firstResponseOfAPI === ids.mbgwSentCatalogueBap &&
           secondResponseOfAPI === ids.mbgwSentCatalogueBap)
       ) {
-        setEvents1(res?.events[1].event);
+        await setEvents1(res?.events[1].event);
       } else {
-        setEvents1([]);
+        await setEvents1([]);
       }
     } catch (error) {
       console.log(`error ${error}`);
@@ -52,11 +54,115 @@ const NodeAsHandleFlow: React.FC = () => {
   useInterval(() => {
     fetchEvent();
   }, 1000);
+  console.log(`events`, events, "lol", events1);
+  const updateNodes = nodes
+    .map((node) => {
+      if (
+        (node.id === ids.mobility &&
+          events?.eventSource?.id === ids.mobility) ||
+        (node.id === ids.mobility && events1?.eventSource?.id === ids.mobility)
+      ) {
+        return {
+          ...node,
+          className: "activeMobility",
+        };
+      }
+      if (
+        (node.id === ids.gateway && events?.eventSource?.id === ids.gateway) ||
+        (node.id === ids.gateway && events1?.eventSource?.id === ids.gateway) ||
+        (node.id === ids.gateway &&
+          events?.eventDestination?.id === ids.gateway) ||
+        (node.id === ids.gateway &&
+          events1?.eventDestination?.id === ids.gateway)
+      ) {
+        return {
+          ...node,
+          className: "activeGateway",
+        };
+      }
+      if (
+        (node.id === ids.taxi && events?.eventSource?.id === ids.taxi) ||
+        (node.id === ids.taxi && events1?.eventSource?.id === ids.taxi)
+      ) {
+        return {
+          ...node,
+          className: "activeTaxi",
+        };
+      }
 
+      if (
+        (node.id === ids.luxeCabs &&
+          events?.eventSource?.id === ids.luxeCabs) ||
+        (node.id === ids.luxeCabs && events1?.eventSource?.id === ids.luxeCabs)
+      ) {
+        return {
+          ...node,
+          className: "activeLuxeCabs",
+        };
+      }
+      if (
+        (node.id === ids.whatsappMobility &&
+          events?.eventSource?.id === ids.whatsappMobility) ||
+        (node.id === ids.whatsappMobility &&
+          events1?.eventSource?.id === ids.whatsappMobility)
+      ) {
+        return {
+          ...node,
+          className: "activeWhatsappMobility",
+        };
+      }
+      if (
+        (node.id === ids.mobility &&
+          events?.eventDestination?.id === ids.mobility) ||
+        (node.id === ids.mobility &&
+          events1?.eventDestination?.id === ids.mobility)
+      ) {
+        return {
+          ...node,
+          className: "destMobility",
+        };
+      }
+      if (
+        (node.id === ids.taxi && events?.eventDestination?.id === ids.taxi) ||
+        (node.id === ids.taxi && events1?.eventDestination?.id === ids.taxi)
+      ) {
+        return {
+          ...node,
+          className: "destTaxi",
+        };
+      }
+      if (
+        (node.id === ids.luxeCabs &&
+          events?.eventDestination?.id === ids.luxeCabs) ||
+        (node.id === ids.luxeCabs &&
+          events1?.eventDestination?.id === ids.luxeCabs)
+      ) {
+        return {
+          ...node,
+          className: "destLuxeCabs",
+        };
+      }
+      if (
+        (node.id === ids.whatsappMobility &&
+          events?.eventDestination?.id === ids.whatsappMobility) ||
+        (node.id === ids.whatsappMobility &&
+          events1?.eventDestination?.id === ids.whatsappMobility)
+      ) {
+        return {
+          ...node,
+          className: "destWhatsappMobility",
+        };
+      }
+      return node;
+    })
+    .filter(Boolean);
+
+  console.log(`updateNodes`, updateNodes);
+  // setNodes(updateNodes);
   return (
     <div className="floatingedges">
       <ReactFlow
-        nodes={nodes}
+        nodes={updateNodes}
         edges={initialEdge}
         onNodesChange={onNodesChange}
         fitView
